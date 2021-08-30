ee37c7c5b5bbfcd25d18981040c69f312b84837e
Claire <clairelogan16@gmail.com>
2021-08-30 08:57:00 -0700

    finished lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = Student.where('first_name = ?', params[:name]).or(Student.where('last_name = ?', params[:name]))
     render json: students
   end
 
+  def by_id
+    student = Student.find(params[:id])
+    render json: student
+  end
+
 end
+
+
